ac3274b00fa3fc45378602528eb6ebb5f29ae84d
Haps <thiamhapstou@gmail.com>
2024-08-06 17:52:11 +0000

    route du logout

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('login', [ApiController::class ,'login'] ); 
+Route::middleware("auth")->group(
+    function () {
+        
 Route::get("logout", [ApiController::class, "logout"]);
 Route::apiResource('etudiants', EtudiantController::class);
 Route::post('etudiants/{id}/restore', [EtudiantController::class, "restore"]);
 Route::delete('etudiants/{id}/force', [EtudiantController::class, "forceDelete"]);
 Route::apiResource('evaluations',EvaluationController::class);
+    }
+);
